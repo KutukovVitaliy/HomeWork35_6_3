013ee4eb9caac5b37f7c16aca8210c1de0512352
kvit <kutukov@list.ru>
2022-04-04 20:14:53 +0300

    next commit

--- main.cpp
+++ main.cpp
@@ -5,9 +5,22 @@
 namespace fs = std::experimental::filesystem;
 
 int main() {
-    for(auto& p: fs::recursive_directory_iterator("/home/kvitadmin/gammu"))
-        if(p.path().extension().compare(".txt") == 0){
-            std::cout << p.path() << '\n';
-        }
+    auto lambdaFunction([](std::string& inPath, std::string& extensionToFind){
+        std::vector<std::string > list;
+        for(auto& p: fs::recursive_directory_iterator(inPath))
+            if(is_regular_file(p.path())) {
+                if (p.path().extension().compare(extensionToFind) == 0) {
+                    //std::cout << p.path() << '\n';
+                    list.emplace_back(p.path());
+                }
+            }
+        return list;
+    });
+    std::string path{"/home/kvit/Modbus"};
+    std::string extension{".txt"};
+    auto extensionList = lambdaFunction(path, extension);
+    for(auto& el : extensionList){
+        std::cout << el << std::endl;
+    }
     return 0;
 }
